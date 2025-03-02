5eb0372ca3b627148f399d1169e9a2d9800a786f
Bryan Munoz <tiagotroller021019@gmail.com>
2025-03-02 16:35:49 -0500

    Primer commit: Configuración inicial del proyecto

---
+++ src/api/proveedore/controllers/proveedore.ts
@@ -0,0 +1,7 @@
+/**
+ * proveedore controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::proveedore.proveedore');
---
+++ src/api/proveedore/services/proveedore.ts
@@ -0,0 +1,7 @@
+/**
+ * proveedore service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::proveedore.proveedore');
